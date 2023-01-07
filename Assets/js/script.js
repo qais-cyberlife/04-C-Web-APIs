1fd5a9254c772e1b6ade81979a1ff109cbd77be6
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-07 15:30:35 -0500

    questions and choices done! :)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,3 @@
-
 var quiz = document.querySelector(".quiz");
 var highscores = document.querySelector(".highscores");
 var timer = document.querySelector(".timer");
@@ -8,79 +7,88 @@ var description = document.querySelector(".quiz-description");
 var startButton = document.querySelector(".start-button");
 
 var choices = document.querySelector(".choices");
+var question = document.querySelector(".question")
+
 var firstChoice = document.createElement("button");
 var secondChoice = document.createElement("button");
 var thirdChoice = document.createElement("button");
 var fourthChoice = document.createElement("button");
 
+var x = 0;
+
 var questions = [
-    { id: 1, label: 'Commonly used data types DO NOT include: ' },  
-    { id: 2, label: 'The condition in an if / else statement is encloded within ___.' },  
-    { id: 3, label: 'Arrays in JavaScript can be used to store ___.' },  
+    { id: 1, label: 'Commonly used data types DO NOT include: ' },
+    { id: 2, label: 'The condition in an if / else statement is encloded within ___.' },
+    { id: 3, label: 'Arrays in JavaScript can be used to store ___.' },
     { id: 4, label: 'String values must be enclosed within  ___ when being assigned variables.' },
-    { id: 5, label: 'A very useful too used during development and debugging for pringing content to the debugger is:'} 
+    { id: 5, label: 'A very useful too used during development and debugging for pringing content to the debugger is:' }
 ];
 
-const buttonList = [  
-    { id: 1, label: '1. Strings' },  
-    { id: 2, label: '2. Booleans' },  
-    { id: 3, label: '3. Alerts' },  
-    { id: 4, label: '4. Numbers' }, 
-];
+const allChoices = new Array(
 
-const choices1 = [  
-    { id: 1, label: '1. Quotes' },  
-    { id: 2, label: '2. Curly Brackets' },  
-    { id: 3, label: '3. Parantheses' },  
-    { id: 4, label: '4. Square Brackets' }, 
-];
+    [
+        { id: 1, label: '1. Strings' },
+        { id: 2, label: '2. Booleans' },
+        { id: 3, label: '3. Alerts' },
+        { id: 4, label: '4. Numbers' },
+    ],
 
-
-const choices2 = [  
-    { id: 1, label: '1. Numbers and Strings' },  
-    { id: 2, label: '2. Other Arrays' },  
-    { id: 3, label: '3. Booleans' },  
-    { id: 4, label: '4. All of the Above' }, 
-];
+    [
+        { id: 1, label: '1. Quotes' },
+        { id: 2, label: '2. Curly Brackets' },
+        { id: 3, label: '3. Parantheses' },
+        { id: 4, label: '4. Square Brackets' },
+    ],
 
 
-const choices3 = [  
-    { id: 1, label: '1. Commas' },  
-    { id: 2, label: '2. Curly Bracket' },  
-    { id: 3, label: '3. Quotes' },  
-    { id: 4, label: '4. Parentheses' }, 
-];
+    [
+        { id: 1, label: '1. Numbers and Strings' },
+        { id: 2, label: '2. Other Arrays' },
+        { id: 3, label: '3. Booleans' },
+        { id: 4, label: '4. All of the Above' },
+    ],
 
 
-const choices4 = [  
-    { id: 1, label: 'Button 1' },  
-    { id: 2, label: 'Button 2' },  
-    { id: 3, label: 'Button 3' },  
-    { id: 4, label: 'Button 4' }, 
-];
+    [
+        { id: 1, label: '1. Commas' },
+        { id: 2, label: '2. Curly Bracket' },
+        { id: 3, label: '3. Quotes' },
+        { id: 4, label: '4. Parentheses' },
+    ],
+
+    [
+        { id: 1, label: '1. JavaScript' },
+        { id: 2, label: '2. Terminal/ Bash' },
+        { id: 3, label: '3. For Loops' },
+        { id: 4, label: '4. console.log' },
+    ]
+)
 
 
-const choices5 = [  
-    { id: 1, label: '1. JavaScript' },  
-    { id: 2, label: '2. Terminal/ Bash' },  
-    { id: 3, label: '3. For Loops' },  
-    { id: 4, label: '4. console.log' }, 
-];
+function firstQuestion() {
+    if (x == 0) {
+        title.remove();
+        description.remove();
+        startButton.remove();  
+    }
+   
+    question.textContent = questions[x].label
+    for (let i = 0; i < allChoices[x].length; i++) {
+        const button = document.createElement('button');
+        const li = document.createElement('li');
+        button.innerHTML = allChoices[x][i].label;
+        button.setAttribute('id', `button-${allChoices[x][i].id}`);
+        choices.appendChild(li);
+        li.appendChild(button)
+    }
 
-
-title.remove();
-description.remove();
-startButton.remove();
-
-
-const buttonContainer = document.querySelector('.choices');
-
-for (let i = 0; i < buttonList.length; i++) {
-  const button = document.createElement('button');
-  const li = document.createElement('li'); 
-  
-  button.innerHTML = buttonList[i].label;
-  button.setAttribute('id', `button-${buttonList[i].id}`);
-  choices.appendChild(li);
-  li.appendChild(button)
+    choices.addEventListener('click', nextQuestion);
 }
+
+function nextQuestion() {
+    choices.innerHTML = "";
+    x++
+    firstQuestion();
+}
+
+startButton.addEventListener('click', firstQuestion);

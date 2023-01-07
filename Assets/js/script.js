e1e02e2ca39c1441271d4f50d4ae3d7ffb9049c3
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-07 18:10:33 -0500

    the questions and timer are working, however it is only showing 3 questions plus it is not prompting the user to store inital and does not store highscores yet

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,49 +21,67 @@ var questions = [
     { id: 2, label: 'The condition in an if / else statement is encloded within ___.' },
     { id: 3, label: 'Arrays in JavaScript can be used to store ___.' },
     { id: 4, label: 'String values must be enclosed within  ___ when being assigned variables.' },
-    { id: 5, label: 'A very useful too used during development and debugging for pringing content to the debugger is:' }
+    { id: 5, label: 'A very useful too used during development and debugging for pringing content to the debugger is:' }, 
+    {id:6, label: ' '}
 ];
 
 const allChoices = new Array(
 
     [
-        { id: 1, label: '1. Strings' },
-        { id: 2, label: '2. Booleans' },
-        { id: 3, label: '3. Alerts' },
-        { id: 4, label: '4. Numbers' },
+        { id: 1, label: '1. Strings', correct: false  },
+        { id: 2, label: '2. Booleans', correct: false },
+        { id: 3, label: '3. Alerts', correct: true },
+        { id: 4, label: '4. Numbers', correct: false },
     ],
 
     [
-        { id: 1, label: '1. Quotes' },
-        { id: 2, label: '2. Curly Brackets' },
-        { id: 3, label: '3. Parantheses' },
-        { id: 4, label: '4. Square Brackets' },
-    ],
-
-
-    [
-        { id: 1, label: '1. Numbers and Strings' },
-        { id: 2, label: '2. Other Arrays' },
-        { id: 3, label: '3. Booleans' },
-        { id: 4, label: '4. All of the Above' },
+        { id: 1, label: '1. Quotes',correct: false },
+        { id: 2, label: '2. Curly Brackets', correct: false },
+        { id: 3, label: '3. Parantheses', correct: true },
+        { id: 4, label: '4. Square Brackets', correct: false },
     ],
 
 
     [
-        { id: 1, label: '1. Commas' },
-        { id: 2, label: '2. Curly Bracket' },
-        { id: 3, label: '3. Quotes' },
-        { id: 4, label: '4. Parentheses' },
+        { id: 1, label: '1. Numbers and Strings', correct: false },
+        { id: 2, label: '2. Other Arrays', correct: false },
+        { id: 3, label: '3. Booleans', correct: false },
+        { id: 4, label: '4. All of the Above', correct: true },
+    ],
+
+
+    [
+        { id: 1, label: '1. Commas', correct: false },
+        { id: 2, label: '2. Curly Bracket', correct: false },
+        { id: 3, label: '3. Quotes', correct: true },
+        { id: 4, label: '4. Parentheses', correct: false },
     ],
 
     [
-        { id: 1, label: '1. JavaScript' },
-        { id: 2, label: '2. Terminal/ Bash' },
-        { id: 3, label: '3. For Loops' },
-        { id: 4, label: '4. console.log' },
+        { id: 1, label: '1. JavaScript', correct: false },
+        { id: 2, label: '2. Terminal/ Bash', correct: false },
+        { id: 3, label: '3. For Loops', correct: false },
+        { id: 4, label: '4. console.log', correct: true },
     ]
 )
 
+var secondsLeft = 60;
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      
+      timer.textContent = "Time: " + secondsLeft;
+      secondsLeft--;
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
 
 function firstQuestion() {
     if (x == 0) {
@@ -77,18 +95,23 @@ function firstQuestion() {
         const button = document.createElement('button');
         const li = document.createElement('li');
         button.innerHTML = allChoices[x][i].label;
-        button.setAttribute('id', `button-${allChoices[x][i].id}`);
+        button.setAttribute('id', `${allChoices[x][i].correct}`);
         choices.appendChild(li);
         li.appendChild(button)
     }
 
-    choices.addEventListener('click', nextQuestion);
-}
+    choices.addEventListener('click', function nextQuestion(event) {
+    var isCorrect = event.target.id;
+    console.log(isCorrect);
 
-function nextQuestion() {
+    if (isCorrect == "false") {
+        secondsLeft = secondsLeft - 10
+    }
     choices.innerHTML = "";
     x++
     firstQuestion();
+});
 }
 
 startButton.addEventListener('click', firstQuestion);
+startButton.addEventListener('click', setTime);
